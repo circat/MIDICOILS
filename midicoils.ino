dc06250505e15e9705843b8b52b948af5c0fb149
erixerix <erichlesovsky@gmail.com>
2018-08-31 17:17:56 +0200

    Create midicoils.ino

---
+++ midicoils.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <MIDI.h>
+
+const int coil1 =  3;
+const int coil2 =  5;
+
+void HandleNoteOn (byte channel, byte number, byte velocity) { 
+  if (number == 36) {
+  analogWrite(coil1, velocity);
+  delay(30);
+  analogWrite(coil1, LOW); 
+  }
+ if (number == 37) {
+  analogWrite(coil2, velocity);
+  delay(10);
+  analogWrite(coil2, LOW);
+  } 
+}   
+void setup()
+{
+    pinMode(coil1, OUTPUT);
+    pinMode(coil2, OUTPUT);
+                    
+    MIDI.begin(MIDI_CHANNEL_OMNI);
+    MIDI.turnThruOff();
+    Serial.begin(115200);
+    MIDI.setHandleNoteOn(HandleNoteOn);
+}
+    
+void loop()
+{
+        MIDI.read(); 
+}
